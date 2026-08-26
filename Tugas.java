0bd2ec3877cc3b293b48c0274dc793e5cd99494b
vandra <vandrawasmb@gmail.com>
2026-08-26 18:48:38 +0700

    Add files via upload

---
+++ Tugas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.vandra_project1;
+
+/**
+ *
+ * @author emyri
+ */
+public class Tugas {
+    public static void main(String[] args) {
+        System.out.println("Nama  :Vandra Noprianus Sembiring !") ;
+        System.out.println("NPM   :25050600006");
+        System.out.println("Alamat:Sanggrahan Wates RT/RW06");
+        System.out.println("No_HP :081396534027");
+        
+    }
+}
---
+++ Vandra_project1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.vandra_project1;
+
+
+public class Vandra_project1 {
+
+        public static void main(String[] args) {
+        System.out.println("Nama  :Vandra Noprianus Sembiring !") ;
+        System.out.println("NPM   :25050600006");
+        System.out.println("Alamat:Sanggrahan Wates RT/RW06");
+        System.out.println("No_HP :081396534027");
+        }
+}
